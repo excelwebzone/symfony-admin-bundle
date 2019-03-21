b4c6b7282c60b6b10d454f8477f270f5e07dc6d0
Michael H. Arieli <michaelharieli@gmail.com>
2019-03-21 11:10:38 -0700

    Added suport for date range in reports

--- src/Report/AbstractReport.php
+++ src/Report/AbstractReport.php
@@ -457,6 +457,138 @@ abstract class AbstractReport
     }
 
     /**
+     * @see \EWZ\SymfonyAdminBundle\Repository\AbstractRepository::getDateRange
+     *
+     * @param string $unit
+     *
+     * @return [\DateTimeInterface, \DateTimeInterface]|null
+     */
+    protected function getDateRange(string $unit): ?array
+    {
+        switch ($unit) {
+            case 'today':
+                $from = (new \DateTime())->setTime(0, 0, 0);
+                $to = (new \DateTime('tomorrow'))->setTime(0, 0, 0);
+
+                return [$from, $to];
+
+            case 'yesterday':
+                $from = (new \DateTime('yesterday'))->setTime(0, 0, 0);
+                $to = (new \DateTime())->setTime(0, 0, 0);
+
+                return [$from, $to];
+
+            case 'last_week':
+                $from = (new \DateTime('previous week'))->setTime(0, 0, 0);
+                $to = (new \DateTime('this week'))->setTime(0, 0, 0);
+
+                return [$from, $to];
+
+            case 'this_week':
+                $from = (new \DateTime('this week'))->setTime(0, 0, 0);
+                $to = (new \DateTime('next week'))->setTime(0, 0, 0);
+
+                return [$from, $to];
+
+            case 'last_month':
+                $from = (new \DateTime('first day of previous month'))->setTime(0, 0, 0);
+                $to = (new \DateTime('first day of this month'))->setTime(0, 0, 0);
+
+                return [$from, $to];
+
+            case 'this_month':
+                $from = (new \DateTime('first day of this month'))->setTime(0, 0, 0);
+                $to = (new \DateTime('first day of next month'))->setTime(0, 0, 0);
+
+                return [$from, $to];
+
+            case 'last_quarter':
+                $from = (new \DateTime())->setTime(0, 0, 0);
+                $to = (clone $from);
+
+                $month = $from->format('n');
+                if ($month < 4) {
+                    $from->modify('first day of last year october');
+                } elseif ($month > 3 && $month < 7) {
+                    $from->modify('first day of january');
+                } elseif ($month > 6 && $month < 10) {
+                    $from->modify('first day of april');
+                } elseif ($month > 9) {
+                    $from->modify('first day of july');
+                }
+
+                if ($month < 4) {
+                    $to->modify('last day of last year december');
+                } elseif ($month > 3 && $month < 7) {
+                    $to->modify('last day of march');
+                } elseif ($month > 6 && $month < 10) {
+                    $to->modify('last day of june');
+                } elseif ($month > 9) {
+                    $to->modify('last day of september');
+                }
+                $to->modify('next day');
+
+                return [$from, $to];
+
+            case 'this_quarter':
+                $from = (new \DateTime())->setTime(0, 0, 0);
+                $to = (clone $from);
+
+                $month = $from->format('n');
+                if ($month < 4) {
+                    $from->modify('first day of january');
+                } elseif ($month > 3 && $month < 7) {
+                    $from->modify('first day of april');
+                } elseif ($month > 6 && $month < 10) {
+                    $from->modify('first day of july');
+                } elseif ($month > 9) {
+                    $from->modify('first day of october');
+                }
+
+                if ($month < 4) {
+                    $to->modify('last day of march');
+                } elseif ($month > 3 && $month < 7) {
+                    $to->modify('last day of june');
+                } elseif ($month > 6 && $month < 10) {
+                    $to->modify('last day of september');
+                } elseif ($month > 9) {
+                    $to->modify('last day of december');
+                }
+                $to->modify('next day');
+
+                return [$from, $to];
+
+            case 'last_year':
+                $from = (new \DateTime('january first day of previous year'))->setTime(0, 0, 0);
+                $to = (new \DateTime('january first day of this year'))->setTime(0, 0, 0);
+
+                return [$from, $to];
+
+            case 'this_year':
+                $from = (new \DateTime('january first day of this year'))->setTime(0, 0, 0);
+                $to = (new \DateTime('january first day of next year'))->setTime(0, 0, 0);
+
+                return [$from, $to];
+
+            case 'last_7_days':
+            case 'last_14_days':
+            case 'last_30_days':
+            case 'last_45_days':
+            case 'last_60_days':
+            case 'last_90_days':
+            case 'last_180_days':
+                $split = explode('_', $unit);
+
+                $from = (new \DateTime(sprintf('-%d day', $split[1])))->setTime(0, 0, 0);
+                $to = (new \DateTime())->setTime(0, 0, 0);
+
+                return [$from, $to];
+        }
+
+        return null;
+    }
+
+    /**
      * @param string $column
      * @param array  $data
      *
