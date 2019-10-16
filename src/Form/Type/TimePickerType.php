451d7391a2afe7b801756aab2c428e209dca190a
Michael H. Arieli <michaelharieli@gmail.com>
2019-10-16 08:00:38 -0700

    Added interval and 24hour options to time picker

--- src/Form/Type/TimePickerType.php
+++ src/Form/Type/TimePickerType.php
@@ -4,10 +4,21 @@ namespace EWZ\SymfonyAdminBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class TimePickerType extends AbstractType
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['time_interval'] = intval($options['interval']);
+        $view->vars['time_24hour'] = intval($options['24hour']);
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -16,6 +27,8 @@ class TimePickerType extends AbstractType
         $resolver->setDefaults([
             'html5' => false,
             'widget' => 'single_text',
+            'interval' => 30,
+            '24hour' => false,
         ]);
     }
 
