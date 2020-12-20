5e4374d145913e56a56e5903dee0b508f702975e
Michael H. Arieli <michaelharieli@gmail.com>
2020-12-20 09:47:16 -0800

    Allow return custom data when deleting object

--- src/Controller/Admin/Api/Traits/DeleteTrait.php
+++ src/Controller/Admin/Api/Traits/DeleteTrait.php
@@ -10,10 +10,11 @@ trait DeleteTrait
      * @param mixed         $object
      * @param \Closure|null $preDelete
      * @param \Closure|null $postDelete
+     * @param \Closure|null $onCompleted
      *
      * @return JsonResponse
      */
-    private function doDelete($object, $preDelete = null, $postDelete = null): JsonResponse
+    private function doDelete($object, $preDelete = null, $postDelete = null, $onCompleted = null): JsonResponse
     {
         if ($preDelete instanceof \Closure) {
             $preDelete->bindTo($this)($object);
@@ -25,9 +26,16 @@ trait DeleteTrait
             $postDelete->bindTo($this)($object);
         }
 
-        return $this->json([
-            'ok' => true,
+        $data = [
+            'id' => $object->getId(),
             'label' => (string) $object,
-        ]);
+        ];
+        if ($onCompleted instanceof \Closure) {
+            $data = array_merge($data, $onCompleted->bindTo($this)($object));
+        }
+
+        return $this->json(array_merge($data, [
+            'ok' => true,
+        ]));
     }
 }
